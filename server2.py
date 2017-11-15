3c65add23eacf7430268d55e68f4161f6b905d01
vagrant <brittanyneal22@gmail.com>
2017-11-15 19:14:43 +0000

    routes

---
+++ server2.py
@@ -0,0 +1,48 @@
+"""Tomagachi server"""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, jsonify, render_template, redirect, request,
+                   flash, session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+# from model import connect_to_db, db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. This fixes that, so instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route('/creation')
+def create():
+    """creation page"""
+
+    return render_template("create.html")
+
+    gachi_name = request.form.get("name")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
