64aa508849be7a4b43b044985211e4d6b4a15731
vagrant <brittanyneal22@gmail.com>
2017-12-02 01:37:32 +0000

    working DB, next task: link server to DB

---
+++ model.py
@@ -0,0 +1,42 @@
+"""Models and database functions for Tomagachi project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """User of Tomagachi website."""
+
+    __tablename__ = "users"
+
+    user_id = db.column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+
+    def __repr__(self):
+        """provide representation when printed"""
+
+        return "User user_id={} email={}>" .format(self.user_id, self.email)
+
+# Question: For storing my class objects in DB, need to store object only,
+# perhaps with unique ID, or need to separate out each attribute value?
+
+
+class Tomagachi(db.Model):
+    """Gachis belonging to user"""
+
+    __tablename__ = "tomagachis"
+
+    gachi_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    name = db.Column(db.String(64), nullable=True)
+    motto = db.Column(db.String(64), nullable=True)
+    favorite_food = db.Column(db.String(64), nullable=True)
+
